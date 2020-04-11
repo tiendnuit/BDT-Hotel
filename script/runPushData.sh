5d609ffaac443c991599ef5f987ab03eb5936ca5
Huy <nguyenvongochuy@gmail.com>
2020-04-10 22:21:44 -0700

    Added scripts

---
+++ script/runPushData.sh
@@ -0,0 +1,1 @@
+sudo sh /home/cloudera/Desktop/Final/scripts/pushData | /home/cloudera/Desktop/Final/kafka_2.11-1.0.0/bin/kafka-console-producer.sh --broker-list localhost:9093 --topic HotelBookingSummary
