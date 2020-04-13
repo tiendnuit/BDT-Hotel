6abd807fd6a390f7abda7aa2a3c23205ad319eac
huy <nguyenvongochuy@gmail.com>
2020-04-12 20:01:41 -0700

    update sql

--- script/Project_Sampe_SQL.sql
+++ script/Project_Sampe_SQL.sql
@@ -1,5 +1,4 @@
 SELECT hotel, COUNT(*) FROM hotel WHERE is_canceled=0 GROUP BY hotel;
 
-
-SELECT hotel, country, COUNT (is_canceled) AS COUNT FROM hotel WHERE is_canceled=0 AND arrival_date_month ='December' GROUP BY hotel, country ORDER BY COUNT DESC LIMIT 10;
+SELECT hotel, country, COUNT (is_canceled) AS count_reservation FROM hotel WHERE is_canceled=0 AND arrival_date_month ='December' GROUP BY hotel, country ORDER BY count_reservation DESC LIMIT 10;
 
